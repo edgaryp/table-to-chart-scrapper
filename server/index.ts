601780ecc0b98795e67582631ab697f2f9ccc97e
Edgar <edgaryp@me.com>
2021-08-07 23:06:42 +1000

    feat: add puppeteer for site crawling

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,18 @@
-const str: string = '';
+import puppeteer from 'puppeteer';
 
-const main = () => {
-  console.log('Hello World');
+const urls: string[] = [
+  'https://en.wikipedia.org/wiki/Women%27s_high_jump_world_record_progression',
+  'https://en.wikipedia.org/wiki/States_and_territories_of_Australia',
+];
+
+const getPageContent = async (url: URL): Promise<string> => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url.href);
+  return await page.content();
 };
 
-main();
+(async () => {
+  const content = await getPageContent(new URL(urls[1]));
+  console.log(content);
+})();
